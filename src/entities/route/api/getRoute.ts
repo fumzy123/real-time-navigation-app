913b694b9ff295e28c48308c685828ec162292aa
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-12-01 09:00:36 -0330

    updated getRoute fetch function

--- src/entities/route/api/getRoute.ts
+++ src/entities/route/api/getRoute.ts
@@ -7,7 +7,17 @@ export async function getRoute(
   const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
 
   const res = await fetch(url);
-  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(`Mapbox API error: ${res.statusText}`)
+  }
 
-  return data.routes?.[0]?.geometry || null;
+  // Get Data
+  const data = await res.json();
+  const bestRoute = data.routes?.[0];
+
+  // Return both Geometry and duration
+  return {
+    geometry: bestRoute?.geometry || null,
+    duration: bestRoute?.duration || null,
+  }
 }
